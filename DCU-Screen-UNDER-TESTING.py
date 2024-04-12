62833d8788a34e0472c059509a8afc6a2bb151d9
Mason Myre <82189404+Lancer077@users.noreply.github.com>
2024-04-12 15:22:15 -0500

    Update DCU-Screen-UNDER-TESTING.py
    fixed holder and stuff

--- DCU-Screen-UNDER-TESTING.py
+++ DCU-Screen-UNDER-TESTING.py
@@ -135,9 +135,9 @@ splash.append(text_group)
 
 #if the CAN queue is larger than a set size, we clear it to ensure we are only getting up to date messages
 def _can_is_full():
-	message_count = listener.in_waiting()
-	if message_count > _can_queue_size:
-		mcp._unread_message_queue.clear()
+    message_count = listener.in_waiting()
+    if message_count > _can_queue_size:
+        mcp._unread_message_queue.clear()
 
 
 #error handling
@@ -166,98 +166,101 @@ runTime = time.time()
 while True:
 
 
-	with mcp.listen(timeout = 0) as listener:
+    with mcp.listen(timeout = 0) as listener:
 
-		_can_is_full()
+        _can_is_full()
 
 
 
-		# Draw Speed/effecency Label
-		text_group = displayio.Group(scale=3, x=3, y=12)
-		text = "S: {:04.1f}".format(mph)
-		text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
-		text_group.append(text_area)  # Subgroup for text scaling
-		splash[-3] = text_group
+        # Draw Speed/effecency Label
+        text_group = displayio.Group(scale=3, x=3, y=12)
+        text = "S: {:04.1f}".format(mph)
+        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
+        text_group.append(text_area)  # Subgroup for text scaling
+        splash[-3] = text_group
 
-		# Draw Current Label
-		text_group = displayio.Group(scale=3, x=3, y=41)
-		text = "A: {:04.1f}".format(current)
-		text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
-		text_group.append(text_area)  # Subgroup for text scaling
-		splash[-2] = text_group
+        # Draw Current Label
+        text_group = displayio.Group(scale=3, x=3, y=41)
+        text = "A: {:04.1f}".format(current)
+        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
+        text_group.append(text_area)  # Subgroup for text scaling
+        splash[-2] = text_group
 
-		# Draw pico/motorcontroller/motor temperature labels
-		text_group = displayio.Group(scale=1, x=15, y=60)
-		text = "P: {:04.1f}  H: {:04.1f}, M: {:04.1f}".format(pico_temp, heatsink_temp, motor_temp)
-		text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
-		text_group.append(text_area)  # Subgroup for text scaling
-		splash[-1] = text_group
-	
-	
-	        message = Message(id=0x6b4, data=struct.pack('<ff',mph,current), extended=False)
-	        send_success = mcp.send(message)
-	        runTime = time.time()
-	        
-	
-	        
-	        #Here starts where we do the CAN things
-	        message_count = listener.in_waiting()
-	        print("message count = {}".format(message_count),end = '\n')
-	        
-	        if message_count == 0:
-	
-	            continue
-	        
-	        next_message = listener.receive()
-	        message_num = 0
-	
-	        
-	        while not next_message is None:
-	
-	            message_num += 1
-	
-	            # Check the id to properly unpack it
-	            if next_message.id == 0x402:
-	
-	            #unpack and print the message
-	                holder = struct.unpack('<ff',next_message.data)
-	                #voltage = holder[0] #we don't need voltage and we are not displaying it
-	                current = holder[1]
-	                #print("Message From: {}: [V = {}; A = {}]".format(hex(next_message.id),voltage,current))
-	
-	
-	
-	            if next_message.id == 0x403:
-	                #unpack and print the message
-	                holder = struct.unpack('<ff',next_message.data)
-	                rpm = holder[0]
-	                #mph = rpm*tire_diameter*math.pi*60*1/(12*5280)
-	                mph = rpm * tire_diameter * 0.003 #rounded a little bit to prevent calculating the extra stuff every time we receive a message
-	                #print("Message From: {}: [rpm = {}; mph = {}]".format(hex(next_message.id),rpm,mph))
-	                
-	            # Recieve tempetaure from heat sink and motor    
-	            if next_message.id == 0x40B:
-	                #unpack and print the message
-	                holder = struct.unpack('<ff',next_message.data)
-	                motor_temp = holder[0]
-	                heatsink_temp = holder[0]
-	                print("Message From: {}: [Motor Temp = {}; Heat Sink = {}]".format(hex(next_message.id),motor_temp,heatsink_temp))
-	
-	            if next_message == 0x40C:
-	                holder = struct.unpack('<ff',next_message.data)
-	                dsp_temp = holder[0]
-	            
-	            if next_message == 0x401:
-	                DCU_timeout = time.monotonic_ns() - prevDCU_time
-	                prevDCU_time = time.monotonic_ns()
-	
-	            
-	
-	
-	            next_message = listener.receive()    
-	
-	
-	
+        # Draw pico/motorcontroller/motor temperature labels
+        pico_temp = microcontroller.cpu.temperature
+        text_group = displayio.Group(scale=1, x=0, y=60)
+        text = "P: {:04.1f}  H: {:04.1f}, M: {:04.1f}".format(pico_temp, heatsink_temp, motor_temp)
+        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
+        text_group.append(text_area)  # Subgroup for text scaling
+        splash[-1] = text_group
+
+
+        message = Message(id=0x6b4, data=struct.pack('<ff',mph,current), extended=False)
+        send_success = mcp.send(message)
+        runTime = time.time()
+        
+
+        
+        #Here starts where we do the CAN things
+        message_count = listener.in_waiting()
+        print("message count = {}".format(message_count),end = '\n')
+        
+        if message_count == 0:
+
+            continue
+        
+        next_message = listener.receive()
+        message_num = 0
+
+        
+        while not next_message is None:
+
+            message_num += 1
+
+            # Check the id to properly unpack it
+            if next_message.id == 0x402:
+
+            #unpack and print the message
+                holder = struct.unpack('<ff',next_message.data)
+                #voltage = holder[0] #we don't need voltage and we are not displaying it
+                current = holder[1]
+                #print("Message From: {}: [V = {}; A = {}]".format(hex(next_message.id),voltage,current))
+
+
+
+            if next_message.id == 0x403:
+                #unpack and print the message
+                holder = struct.unpack('<ff',next_message.data)
+                rpm = holder[0]
+                #mph = rpm*tire_diameter*math.pi*60*1/(12*5280)
+                mph = rpm * tire_diameter * 0.003 #rounded a little bit to prevent calculating the extra stuff every time we receive a message
+                #print("Message From: {}: [rpm = {}; mph = {}]".format(hex(next_message.id),rpm,mph))
+                
+            # Recieve tempetaure from heat sink and motor    
+            if next_message.id == 0x40B:
+                #unpack and print the message
+                holder = struct.unpack('<ff',next_message.data)
+                motor_temp = holder[0]
+                heatsink_temp = holder[1]
+                print("Message From: {}: [Motor Temp = {}; Heat Sink = {}]".format(hex(next_message.id),motor_temp,heatsink_temp))
+
+            if next_message == 0x40C:
+                holder = struct.unpack('<ff',next_message.data)
+                dsp_temp = holder[0]
+            
+            if next_message == 0x401:
+                DCU_timeout = time.monotonic_ns() - prevDCU_time
+                prevDCU_time = time.monotonic_ns()
+
+            
+
+
+            next_message = listener.receive()    
+
+
+
+
+
 
 
 
